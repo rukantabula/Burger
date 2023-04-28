84f495e63ca96bf1e27e8346d785dd0c1b383a5b
benson <benson.rukantabula@lego.com>
2023-04-28 10:49:03 +0200

    Send react vitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { render } from 'react-dom';
+import reactVitalsService from './service/react-vitals-service';
 import './index.css';
 
 document.title = 'Burger Fanatics';
@@ -15,8 +16,7 @@ render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-const sendMetrics = (metric: string) => {
-  // send JSON.stringify(metric) payload to me for monitoring
-}
 
-reportWebVitals(sendMetrics);
+
+// send JSON.stringify(metric) payload to me for monitoring
+reportWebVitals(reactVitalsService.sendMetrics);
---
+++ src/service/react-vitals-service.ts
@@ -0,0 +1,9 @@
+class ReactVitalsService {
+
+    sendMetrics(metrics: string) {
+        // Send vitals to backend or elastic
+    }
+}
+
+const reactVitalsService: ReactVitalsService = new ReactVitalsService();
+export default reactVitalsService;
